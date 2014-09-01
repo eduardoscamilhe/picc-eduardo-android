caa2fc4db0abaab0eb423c14f712e00ee9353346
eduardoscamilhe <eduardo.scamilhe@gmail.com>
2014-09-01 20:32:39 -0300

    Comitando pela primeira vez ;D

---
+++ 01-09-2014/treinamento_app/src/br/com/treinamento/app/MeuOnClickListener.java
@@ -0,0 +1,14 @@
+package br.com.treinamento.app;
+
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MeuOnClickListener implements OnClickListener {
+
+	@Override
+	public void onClick(View view) {
+		Log.d("MEU DEBUG", "CLICOU PRA COROLHOS!");
+	}
+
+}
